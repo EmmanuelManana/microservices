2fa2fdbe58c3399fed50108c36024962ea65b3ea
EmmanuelManana <emanana@student.wethinkcode.co.za>
2020-12-24 21:19:58 +0200

    complete

--- restapi-users/src/config/config.ts
+++ restapi-users/src/config/config.ts
@@ -1,13 +1,15 @@
+require('dotenv').config();
+
 export const config = {
   "dev": {
     "username": process.env.POSTGRESS_USERNAME,
     "password": process.env.POSTGRESS_PASSWORD,
-    "database": process.env.POSTGRESS_DATABASE,
+    "database": process.env.POSTGRESS_DB,
     "host": process.env.POSTGRESS_HOST,
     "dialect": "postgres",
     "aws_region":  process.env.AWS_REGION,
     "aws_profile": process.env.AWS_PROFILE,
-    "aws_media_bucket": process.env.AWS_MEDIA_BUCKET
+    "aws_media_bucket": process.env.AWS_BUCKET
   },
   "prod": {
     "username": "",
@@ -17,6 +19,8 @@ export const config = {
     "dialect": "postgres"
   },
   "jwt": {
-    "secret": process.env.JWT_SECRET
+    "secret": "hello word"
   }
 }
+
+// "secret": process.env.JWT_SECRET
